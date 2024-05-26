42b0afa91130a51a0baa486ad064ee672a5773f2
Maxim Krisanov <maximkrisan@icloud.com>
2024-05-26 19:08:22 +0300

    Adding a description for Swagger.

--- src/main/java/com/krisanov/codenest/lesson/controller/LessonController.java
+++ src/main/java/com/krisanov/codenest/lesson/controller/LessonController.java
@@ -1,44 +1,56 @@
 package com.krisanov.codenest.lesson.controller;
 
-import com.krisanov.codenest.lesson.dto.LessonResponseDto;
+import com.krisanov.codenest.common.dto.ResponseErrorDto;
+import com.krisanov.codenest.lesson.dto.LessonDto;
 import com.krisanov.codenest.lesson.service.LessonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * The controller for the Lesson resource.
- *
- * <p>
- * This controller manages the HTTP request mappings for the Lesson resource. It particularly supports
- * HTTP GET request to fetch a specific Lesson by its identifier. The returned object is a DTO representation
- * of the Lesson.
- * </p>
- *
- * @author Maxim Krisanov
- * @see com.krisanov.codenest.lesson.dto.LessonResponseDto
- * @see com.krisanov.codenest.lesson.service.LessonService
- */
+@Tag(name = "Lesson that the user learns")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/lesson")
 public class LessonController {
 
-    /**
-     * Service that contains business logic for handling Lesson information.
-     */
     private final LessonService lessonService;
 
-    /**
-     * Retrieves a single Lesson with specified identifier.
-     *
-     * @param lessonId the unique identifier of the Lesson to retrieve
-     * @return LessonResponseDto representation of the Lesson
-     */
+    @Operation(
+            description = "User receiving a lesson.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = LessonDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "You must be logged in to perform this action.",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseErrorDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "In case the lesson is not found.",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseErrorDto.class)
+                            )
+                    ),
+            },
+            security = @SecurityRequirement(name = "Bearer JWT")
+    )
     @GetMapping("/{lessonId:\\d+}")
-    public LessonResponseDto getLessonById(@PathVariable Long lessonId) {
+    public LessonDto getLessonById(@PathVariable Long lessonId) {
         return lessonService.findById(lessonId);
     }
 }
--- src/main/java/com/krisanov/codenest/lesson/controller/LessonsController.java
+++ src/main/java/com/krisanov/codenest/lesson/controller/LessonsController.java
@@ -1,55 +1,73 @@
 package com.krisanov.codenest.lesson.controller;
 
-import com.krisanov.codenest.lesson.dto.PageLessonResponseDto;
+import com.krisanov.codenest.common.dto.ResponseErrorDto;
+import com.krisanov.codenest.lesson.dto.LessonDto;
+import com.krisanov.codenest.lesson.dto.PageLessonDto;
 import com.krisanov.codenest.lesson.service.LessonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Controller for handling requests related to multiple Lesson resources.
- *
- * <p>
- * This controller manages HTTP request mappings related to operations to retrieve a batch of
- * Lesson resources. This includes fetching a paginated feed and fetching all lessons in a paginated format.
- * </p>
- *
- * @author Maxim Krisanov
- * @see com.krisanov.codenest.lesson.service.LessonService
- * @see com.krisanov.codenest.lesson.dto.PageLessonResponseDto
- */
+@Tag(name = "Lessons provided to the user")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/lessons")
 public class LessonsController {
 
-    /**
-     * Service that contains business logic for handling Lesson information.
-     */
     private final LessonService lessonService;
 
-    /**
-     * Fetches a paginated feed of Lessons.
-     *
-     * @param pageable defines the pagination parameters
-     * @return a page containing instances of PageLessonResponseDto
-     */
+    @Operation(
+            description = "Lessons presented as recommendations to the user.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = PageLessonDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "In case the lessons are not found.",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseErrorDto.class)
+                            )
+                    ),
+            }
+    )
     @GetMapping("/feed")
-    public Page<PageLessonResponseDto> getFeed(Pageable pageable) {
+    public Page<PageLessonDto> getFeed(@ParameterObject Pageable pageable) {
         return lessonService.findFeed(pageable);
     }
 
-    /**
-     * Retrieves all Lessons in a paginated format.
-     *
-     * @param pageable defines the pagination parameters
-     * @return a page containing instances of PageLessonResponseDto
-     */
+    @Operation(
+            description = "Full list of lessons.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = LessonDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "In case the lessons are not found.",
+                            content = @Content(
+                                    schema = @Schema(implementation = ResponseErrorDto.class)
+                            )
+                    ),
+            }
+    )
     @GetMapping
-    public Page<PageLessonResponseDto> getAllLessons(Pageable pageable) {
+    public Page<PageLessonDto> getAllLessons(@ParameterObject Pageable pageable) {
         return lessonService.findAll(pageable);
     }
 }
