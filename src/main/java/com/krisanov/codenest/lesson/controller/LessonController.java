4f77da3aa0ca09d269ab9385d5212479a41473dd
Maxim Krisanov <maximkrisan@icloud.com>
2024-06-06 20:05:50 +0300

    Changing the controller request method

--- src/main/java/com/krisanov/codenest/lesson/controller/LessonController.java
+++ src/main/java/com/krisanov/codenest/lesson/controller/LessonController.java
@@ -16,9 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -120,7 +120,7 @@ public class LessonController {
             security = @SecurityRequirement(name = "Bearer JWT")
     )
     @PreAuthorize("hasRole('ADMIN')")
-    @PatchMapping("/{lessonId:\\d+}")
+    @PutMapping("/{lessonId:\\d+}")
     public LessonResponse updateLessonById(@PathVariable Long lessonId,
                                            @RequestBody LessonRequest lessonRequest) {
         return lessonService.updateById(lessonId, lessonRequest);
